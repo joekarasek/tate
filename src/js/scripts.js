c40fc3058ce08d4868e00b77b27f622f992d3b53
Joe Karasek <joseph.karasek@gmail.com>
2017-05-23 14:40:20 -0700

    add a team carousel

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -8,63 +8,53 @@
   // ======= Request Appointment Form =======
   HelloWorldDevsTysonSteele.requestAppointment('#mail-form', '#success_msg' , '7fb35345-752d-4792-9480-cd3db6674a62');
 
-  // ======= Google Maps =======
-  HelloWorldDevsTysonSteele.googleMap('#google-map5', '33.8646783,-84.4668918', '33.8646783,-84.4668918');
-
   // ======= Carousels =======
   HelloWorldDevsTysonSteele.marqueeCarousel({
     autoplay: 6000,
     effect: 'fade',
     speed: 500
   });
-  HelloWorldDevsTysonSteele.tourCarousel('.js-tour-carousel', {
-    items: 1,
-    navText: [
-      '<i class="icon-chevron-left"></i>',
-      '<i class="icon-chevron-right"></i>'
-    ]
-  });
-  HelloWorldDevsTysonSteele.pyramidCarousel('.js-associations-carousel', {
-    nav: false,
+  // HelloWorldDevsTysonSteele.tourCarousel('.js-tour-carousel', {
+  //   items: 1,
+  //   navText: [
+  //     '<i class="icon-chevron-left"></i>',
+  //     '<i class="icon-chevron-right"></i>'
+  //   ]
+  // });
+  //
+
+  HelloWorldDevsTysonSteele.pyramidCarousel('.js-team-carousel', {
     items: 2,
-    responsive: {
-      480: {
-        items: 2
-      },
-      768: {
-        items: 3
-      },
-      992: {
-        items: 4
-      }
-    }
-  });
-  HelloWorldDevsTysonSteele.pyramidCarousel('.js-review-carousel', {
-    items: 1,
-    autoplay: true,
-    autoplaySpeed: 400,
-    autoplayTimeout: 6000,
-    autoplayHoverPause:false,
-    nav: true,
-    dots: false,
-    margin: 40,
     navText: [
-      '<img src="assets/review-arrow.png"/>',
-      '<img src="assets/review-arrow__next.png"/>'
+      '<img src="assets/team-arrow-left.png">',
+      '<img src="assets/team-arrow-right.png">'
     ],
     responsive: {
+      480: {
+        items: 3
+      },
       768: {
-        items: 2,
-        dots: true
+        items: 4
       },
       992: {
-        items: 3,
-        dots: true
+        items: 5
+      },
+      1200: {
+        items: 6
       }
-    }
+    },
+    margin: 10
   });
   HelloWorldDevsTysonSteele.updateCopyright('.js-copyright-year');
 
+  // Remove last name from team cards
+  $('.team-card__title').each(function() {
+    var firstWord = $(this).text().split(' ');
+    $(this).text(firstWord[0]);
+  });
+
+  // ======= Google Maps =======
+  HelloWorldDevsTysonSteele.googleMap('#google-map5', '33.8646783,-84.4668918', '33.8646783,-84.4668918');
 
 
 }(jQuery, HelloWorldDevsTysonSteele));
